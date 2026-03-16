f9a1e79cb2e30c07e3c431d735c8021844d3fd84
jjcrouch <105951404+jjcrouch@users.noreply.github.com>
2026-03-16 16:41:40 -0400

    task1 calc distance from median per image - bugfix

--- stitching.py
+++ stitching.py
@@ -139,7 +139,7 @@ def stitch_background(imgs: Dict[str, torch.Tensor]):
         # Determine which image contains the background at foreground mask pixels
         # Use each img's distance from the global median
         all_pixels = torch.cat([img1.reshape(c, -1), img2.reshape(c, -1)], dim=1)
-        rgb_medians = torch.median(all_pixels, dim=1)[0]
+        rgb_medians = torch.median(all_pixels, dim=1)[0].view(-1, 1, 1)
         dist1 = torch.norm(warp_img1 - rgb_medians, dim=0)
         dist2 = torch.norm(warp_img2 - rgb_medians, dim=0)
         print(dist1)
